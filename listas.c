adbb5e37a0530bdc5a0e120a767f38825103a669
tikissmikiss <josehercepreciado@hotmail.com>
2020-05-08 05:18:07 +0200

    ok
    Signed-off-by: tikissmikiss <josehercepreciado@hotmail.com>

--- listas.c
+++ listas.c
@@ -14,81 +14,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Estructura elementos 
-typedef struct _Element {
-    int key;                // Clave
-    struct _Element* next;  // Puntero a elemento siguiente
+// Estructura elementos
+typedef struct _Element
+{
+    int key;               // Clave
+    struct _Element *next; // Puntero a elemento siguiente
 } Element;
 
 // Inicializacion de lista (Creador)
-void inicialize_list (Element** p_head, Element** p_tail) {
+void inicialize_list(Element **p_head, Element **p_tail)
+{
     *p_head = NULL;
     *p_tail = NULL;
 }
 
 // Añadir al principio de la lista
-void add_first(Element** p_head, Element** p_tail, int value) {
-    // Reserva memoria para el nuevo elemento 
-    Element* new_element = malloc(sizeof(Element));   
-    
-    new_element->key = value;       // Carga dato en el elemento
-    new_element->next = *p_head;    // Carga puntero a elemento siguiente
-    
+void add_first(Element **p_head, Element **p_tail, int value)
+{
+    // Reserva memoria para el nuevo elemento
+    Element *new_element = malloc(sizeof(Element));
+
+    new_element->key = value;    // Carga dato en el elemento
+    new_element->next = *p_head; // Carga puntero a elemento siguiente
+
     *p_head = new_element;
-    
-    if (*p_tail==NULL) {
+
+    if (*p_tail == NULL)
+    {
         *p_tail = new_element;
     }
 }
 
 // Añadir al final de la lista
-void add_last(Element** p_head, Element** p_tail, int value) {
-    // Reserva memoria para el nuevo elemento 
-    Element* new_element = malloc(sizeof(Element));   
-    
-    new_element->key = value;       // Carga dato en el elemento
-    new_element->next = NULL;    // Carga .............
-    
-    if (*p_tail==NULL) {
+void add_last(Element **p_head, Element **p_tail, int value)
+{
+    // Reserva memoria para el nuevo elemento
+    Element *new_element = malloc(sizeof(Element));
+
+    new_element->key = value; // Carga dato en el elemento
+    new_element->next = NULL; // Carga .............
+
+    if (*p_tail == NULL)
+    {
         *p_head = *p_tail = new_element;
-    } else {
+    }
+    else
+    {
         (*p_tail)->next = new_element; //OjO parantesis
         *p_tail = new_element;
     }
 }
 
-void delete(Element** p_head, Element** p_tail, Element* abolish) {
+void delete (Element **p_head, Element **p_tail, Element *abolish)
+{
     //Primera posicion
-    if (*p_head==abolish) {
-        if (*p_tail==*p_head) {
+    if (*p_head == abolish)
+    {
+        if (*p_tail == *p_head)
+        {
             *p_head = *p_tail = NULL;
-        } else {
+        }
+        else
+        {
             *p_head = (*p_head)->next;
         }
-    //Segunda en adelante
-    } else {
-        Element* prev = *p_head;
-        while (prev->next!=abolish) {
+        //Segunda en adelante
+    }
+    else
+    {
+        Element *prev = *p_head;
+        while (prev->next != abolish)
+        {
             prev = prev->next;
         }
-        if (prev->next == *p_tail) {
+        if (prev->next == *p_tail)
+        {
             *p_tail = prev;
         }
         prev->next = abolish->next;
     }
-    free(abolish);    
+    free(abolish);
 }
 
-// Imprimir lista 
-void print_list (Element* head) {
-    while (head!=NULL) {
+// Imprimir lista
+void print_list(Element *head)
+{
+    while (head != NULL)
+    {
         printf("%d", head->key);
         head = head->next;
     }
 }
 
-Element* search(Element* head, int value) {
-    while (head!=NULL) {
+Element *search(Element *head, int value)
+{
+    while (head != NULL)
+    {
         if (head->key == value)
             return head;
         head = head->next;
@@ -96,25 +117,32 @@ Element* search(Element* head, int value) {
     return NULL;
 }
 
- void insert_pos (Element** p_head, Element** p_tail, Element* pos, int value) {
-    // Reserva memoria para el nuevo elemento 
-    Element* new_element = malloc(sizeof(Element));   
-    
+void insert_pos(Element **p_head, Element **p_tail, Element *pos, int value)
+{
+    // Reserva memoria para el nuevo elemento
+    Element *new_element = malloc(sizeof(Element));
+
     new_element->key = value;
 
     //Primera posicion
-    if (*p_head==pos) {
+    if (*p_head == pos)
+    {
         new_element->next = pos;
         //Actualizar puntero al primero
         *p_head = new_element;
-    //Ultima Posicion
-    } else if (*p_tail==pos) {
-        add_last(*p_head, *p_tail, value);
+        //Ultima Posicion
+    }
+    else if (*p_tail == pos)
+    {
+        add_last(&*p_head, &*p_tail, value);
         return;
-    //De segunda a penultima
-    } else {
-        Element* prev = *p_head;
-        while (prev->next!=pos) {
+        //De segunda a penultima
+    }
+    else
+    {
+        Element *prev = *p_head;
+        while (prev->next != pos)
+        {
             prev = prev->next;
         }
         prev->next = new_element;
@@ -122,32 +150,38 @@ Element* search(Element* head, int value) {
     }
 }
 
- void insert_ord (Element** p_head, Element** p_tail, int value) {
-    // Reserva memoria para el nuevo elemento 
-    Element* new_element = malloc(sizeof(Element));   
-    
+void insert_ord(Element **p_head, Element **p_tail, int value)
+{
+    // Reserva memoria para el nuevo elemento
+    Element *new_element = malloc(sizeof(Element));
+
     new_element->key = value;
 
     //Buscar posicion
-    Element* pos = *p_head;
-    while (pos->key < value-1 && pos->next!=NULL) {
+    Element *pos = *p_head;
+    while (pos->key < value - 1 && pos->next != NULL)
+    {
         pos = pos->next;
     }
 
     //Primera posicion
-    if (*p_head==pos) {
+    if (*p_head == pos)
+    {
         new_element->next = pos;
         //Actualizar puntero al primero
         *p_head = new_element;
-    //Ultima Posicion
-    // } else if (*p_tail==pos) {
+        //Ultima Posicion
+        // } else if (*p_tail==pos) {
 
-    //     add_last(*p_head, *p_tail, value);
-    //     return;
-    //De segunda a penultima
-    } else {
-        Element* prev = *p_head;
-        while (prev->next!=pos) {
+        //     add_last(*p_head, *p_tail, value);
+        //     return;
+        //De segunda a penultima
+    }
+    else
+    {
+        Element *prev = *p_head;
+        while (prev->next != pos)
+        {
             prev = prev->next;
         }
         prev->next = new_element;
@@ -155,10 +189,11 @@ Element* search(Element* head, int value) {
     }
 }
 
-int main(int argc, char** argv) {
-    Element* head;
-    Element* tail;
-    
+int main(int argc, char **argv)
+{
+    Element *head;
+    Element *tail;
+
     inicialize_list(&head, &tail);
 
     add_first(&head, &tail, 2);
@@ -173,10 +208,10 @@ int main(int argc, char** argv) {
     insert_ord(&head, &tail, 7);
 
     // Borrar a partir de valor
-    Element* pos = search(head, 2);
-    delete(&head, &tail, pos);
-    delete(&head, &tail, head);
-    delete(&head, &tail, tail);
+    Element *pos = search(head, 2);
+    delete (&head, &tail, pos);
+    delete (&head, &tail, head);
+    delete (&head, &tail, tail);
 
     //return 0;
 
